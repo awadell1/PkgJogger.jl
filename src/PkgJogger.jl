af9815b966a291426ce87a77eb69034a2facefc5
Alexius Wadell <awadell@gmail.com>
2021-08-13 16:48:52 -0400

    feat: Use loaded modules to access code from Joggers
    Prior versions required that JOGGER_PKGS could be loaded from Main
    This fixes that by getting them from `Base.loaded_modules`
    
    Added test to verify that we do load (via `using`) all packages in
    JOGGER_PKGS

--- src/PkgJogger.jl
+++ src/PkgJogger.jl
@@ -2,6 +2,7 @@ module PkgJogger
 
 using MacroTools
 using BenchmarkTools
+using Revise
 using CodecZlib
 using JSON
 using Pkg
@@ -10,15 +11,16 @@ using Dates
 
 export @jog
 
+import Base: PkgId
 """
-Additional Packages that are required by modules created with [`@jog`](@ref)
+Packages that are required by modules created with [`@jog`](@ref)
 
-[`PkgJogger.ci`](@ref) will add these to the benchmarking environment automatically
+Generated modules will access these via `Base.loaded_modules`
 """
 const JOGGER_PKGS = [
-    PackageSpec(name="PkgJogger", uuid="10150987-6cc1-4b76-abee-b1c1cbd91c01", path=dirname(@__DIR__)),
-    PackageSpec(name="BenchmarkTools", uuid="6e4b80f9-dd63-53aa-95a3-0cdb28fa8baf"),
-    PackageSpec(name="Revise", uuid="295af30f-e4ad-537b-8983-00126c2a3abe"),
+    Base.identify_package(@__MODULE__, string(@__MODULE__)),
+    PkgId(UUID("6e4b80f9-dd63-53aa-95a3-0cdb28fa8baf"), "BenchmarkTools"),
+    PkgId(UUID("295af30f-e4ad-537b-8983-00126c2a3abe"), "Revise"),
 ]
 
 include("dispatch.jl")
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,15 @@ using SafeTestsets
 using PkgJogger
 
 @testset "PkgJogger.jl" begin
+    # Check that all modules listed in PkgJogger.JOGGER_PKGS are loaded
+    # Do this first so later loadings don't pollute loaded_modules
+    @testset "Loaded Modules" begin
+        @testset "Check $(m.name) was loaded" for m in PkgJogger.JOGGER_PKGS
+            @test haskey(Base.loaded_modules, m)
+        end
+    end
+
+    # Run the rest of the unit testing suite
     @safetestset "Smoke Tests" begin include("smoke.jl") end
     @safetestset "CI Workflow" begin include("ci.jl") end
 end
