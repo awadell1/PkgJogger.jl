7945c9157b574a3549b2fe7b919d7c2403d08b67
Alexius Wadell <awadell@gmail.com>
2021-10-04 22:08:40 -0400

    fix: drop ci on self, and cleanup ci on Example.jl
    Running ci on PkgJogger is creates issues (Like trying to add PkgJogger twice)
    
    Instead just test Example.jl (Tbh this is the right way™️)
    
    Other tweaks
    - Copy  pkg_dir to /tmp to avoid write permission in the ~/.julia/packages
    - Consolidate test_ci_output and run_ci_workflow
    - Remove silly benchmarks from benchmarks/
    - Remove Glob.jl from test/Project.toml
    - Add tests to check that timer benchmarks are decent

--- benchmark/bench_test.jl
+++
@@ -1,5 +0,0 @@
-using BenchmarkTools
-
-suite = BenchmarkGroup()
-suite["sin"] = @benchmarkable sin(rand())
-suite["sincos"] = @benchmarkable sincos(rand())
--- test/ci.jl
+++ test/ci.jl
@@ -1,12 +1,13 @@
 using Test
 using PkgJogger
-using Glob
 
 include("utils.jl")
 
 function run_ci_workflow(pkg_dir)
     # Create temporary default project
     mktempdir() do temp_project
+        # Copy pkg_dir to temp_project
+        cp(pkg_dir, temp_project; force=true)
 
         # Construct CI Command
         cmd = Cmd([
@@ -24,7 +25,7 @@ function run_ci_workflow(pkg_dir)
        # Capture stdout and stderror
         cmd_stdout =  IOBuffer(;append=true)
         cmd_stderr = IOBuffer(;append=true)
-        cmd = Cmd(cmd; dir=pkg_dir)
+        cmd = Cmd(cmd; dir=temp_project)
         cmd = pipeline(cmd; stdout=cmd_stdout, stderr=cmd_stderr)
 
         # Run workflow and return output
@@ -35,37 +36,35 @@ function run_ci_workflow(pkg_dir)
             error("$cmd exited with $proc.exitcode")
         end
 
-        return proc, cmd_stdout, cmd_stderr
+        # Check if benchmark results were saved
+        logs = read(cmd_stderr, String)
+        m = match(r"Saved benchmarks to (.*)\n", logs)
+        m !== nothing || print(logs)
+        @test m !== nothing
+
+        @test length(m.captures) == 1
+        results_file = m.captures[1]
+        @test isfile(results_file)
+
+        # Check that results are in the right place
+        test_subfile(temp_project, results_file)
+        trial_dir = joinpath(PkgJogger.benchmark_dir(temp_project), "trial")
+        test_subfile(trial_dir, results_file)
+
+        # Check that results file is valid
+        results = PkgJogger.load_benchmarks(results_file)
+        test_loaded_results(results)
+
+        # Further checks
+        return results
     end
 end
 
-function test_ci_output(proc, cmd_stdout, cmd_stderr)
-    # Check if benchmark results were saved
-    logs = read(cmd_stderr, String)
-    m = match(r"Saved benchmarks to (.*)\n", logs)
-    m !== nothing || print(logs)
-    @test m !== nothing
-
-    @test length(m.captures) == 1
-    results_file = m.captures[1]
-    @test isfile(results_file)
-
-    # Check that results file is valid
-    results = PkgJogger.load_benchmarks(results_file)
-    test_loaded_results(results)
-    results_file
-end
-
-@testset "PkgJogger.jl" begin
-    proc, cmd_stdout, cmd_stderr = run_ci_workflow(PKG_JOGGER_PATH)
-    results_file = test_ci_output(proc, cmd_stdout, cmd_stderr)
-    @test all( ("benchmark", "trial") .== splitpath(results_file)[end-2:end-1] )
-end
-
-@testset "Unregistered Package" begin
+@testset "Example.jl" begin
     project = joinpath(@__DIR__, "Example.jl")
-    proc, cmd_stdout, cmd_stderr = run_ci_workflow(project)
-    results_file = test_ci_output(proc, cmd_stdout, cmd_stderr)
-    trial_dir = joinpath(PkgJogger.benchmark_dir(project), "trial")
-    test_subfile(trial_dir, results_file)
+    results = run_ci_workflow(project)
+
+    # Check timer results are decent (sleep isn't very accurate)
+    isapprox((time∘minimum)(results["benchmarks"][["timer", "1ms"]]), 1e6; atol=3e6)
+    isapprox((time∘minimum)(results["benchmarks"][["timer", "2ms"]]), 2e6; atol=3e6)
 end
